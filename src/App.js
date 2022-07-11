198c88882c9a031d2fb1c88e127fe522fe06e0b9
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2022-07-11 11:25:16 +0530

    Fixed Intendations

--- src/App.js
+++ src/App.js
@@ -7,29 +7,30 @@ function App() {
   const [atlasData, setAtlasData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/atlasriskversions").then(
-      (response) => {
-        response.json().then((data) => {
-          console.log(data);
-          setAtlasData(data);
-        });
-      }
-    );
+    fetch("http://localhost:8080/atlasriskversions").then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        setAtlasData(data);
+      });
+    });
   }, []);
 
   return (
     <div className="App">
+      {/* {<!--Top NavBar-->} */}
+      <div class="topnav">
+        <img
+          src="https://www.retailbankerinternational.com/wp-content/uploads/sites/2/2020/07/Barclays-logo.jpg"
+          alt="Italian Trulli"
+          class="logo"
+        />
+      </div>
 
-    {/* {<!--Top NavBar-->} */}
-    <div class="topnav">
-      <img src="https://www.retailbankerinternational.com/wp-content/uploads/sites/2/2020/07/Barclays-logo.jpg" alt="Italian Trulli" class="logo"/>
-    </div>
+      {/* {<!--Title-->} */}
+      <label class="title"> Atlas Risk Versions </label>
 
-    {/* {<!--Title-->} */}
-    <label class="title"> Atlas Risk Versions </label>
-
-    {/* {<!--Divider-->} */}
-    <hr class="solid" />
+      {/* {<!--Divider-->} */}
+      <hr class="solid" />
 
       {/* <!--Table--> */}
       <div className="tablecontainer">
@@ -44,9 +45,16 @@ function App() {
           </thead>
           <tbody>
             {/* <!--First Row--> */}
-            {atlasData.length > 0 && atlasData.map((d, ind) => {
-              return <Row key={ind} region={d.region} environment={d.environment} />;
-            })}
+            {atlasData.length > 0 &&
+              atlasData.map((d, ind) => {
+                return (
+                  <Row
+                    key={ind}
+                    region={d.region}
+                    environment={d.environment}
+                  />
+                );
+              })}
           </tbody>
         </table>
       </div>
